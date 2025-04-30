9174825fa099c68619b7a5efd6a9eca294503324
Dpanov2302 <dpanov2302@gmail.com>
2025-04-30 12:49:01 +0300

    models-onnx/yolo: COCO_CLASSES в отдельный файл

--- models/models-onnx/app.py
+++ models/models-onnx/app.py
@@ -9,6 +9,8 @@ import logging
 from pydantic import BaseModel
 import base64
 
+from coco_classes import COCO_CLASSES
+
 app = FastAPI()
 logger = logging.getLogger("uvicorn.error")
 
@@ -77,87 +79,6 @@ async def detect_image_annotated(file: UploadFile = File(...)):
         confidences = []
         class_ids = []
 
-        COCO_CLASSES = {1: 'person',
-                        2: 'bicycle',
-                        3: 'car',
-                        4: 'motorcycle',
-                        5: 'airplane',
-                        6: 'bs',
-                        7: 'train',
-                        8: 'trck',
-                        9: 'boat',
-                        10: 'traffic light',
-                        11: 'fire hydrant',
-                        12: 'stop sign',
-                        13: 'parking meter',
-                        14: 'bench',
-                        15: 'bird',
-                        16: 'cat',
-                        17: 'dog',
-                        18: 'horse',
-                        19: 'sheep',
-                        20: 'cow',
-                        21: 'elephant',
-                        22: 'bear',
-                        23: 'zebra',
-                        24: 'giraffe',
-                        25: 'backpack',
-                        26: 'mbrella',
-                        27: 'handbag',
-                        28: 'tie',
-                        29: 'sitcase',
-                        30: 'frisbee',
-                        31: 'skis',
-                        32: 'snowboard',
-                        33: 'sports ball',
-                        34: 'kite',
-                        35: 'baseball bat',
-                        36: 'baseball glove',
-                        37: 'skateboard',
-                        38: 'srfboard',
-                        39: 'tennis racket',
-                        40: 'bottle',
-                        41: 'wine glass',
-                        42: 'cp',
-                        43: 'fork',
-                        44: 'knife',
-                        45: 'spoon',
-                        46: 'bowl',
-                        47: 'banana',
-                        48: 'apple',
-                        49: 'sandwich',
-                        50: 'orange',
-                        51: 'broccoli',
-                        52: 'carrot',
-                        53: 'hot dog',
-                        54: 'pizza',
-                        55: 'dont',
-                        56: 'cake',
-                        57: 'chair',
-                        58: 'coch',
-                        59: 'potted plant',
-                        60: 'bed',
-                        61: 'dining table',
-                        62: 'toilet',
-                        63: 'tv',
-                        64: 'laptop',
-                        65: 'mose',
-                        66: 'remote',
-                        67: 'keyboard',
-                        68: 'cell phone',
-                        69: 'microwave',
-                        70: 'oven',
-                        71: 'toaster',
-                        72: 'sink',
-                        73: 'refrigerator',
-                        74: 'book',
-                        75: 'clock',
-                        76: 'vase',
-                        77: 'scissors',
-                        78: 'teddy bear',
-                        79: 'hair drier',
-                        80: 'toothbrush'}
-
         for row in output:
             conf = row[4]
             if conf > 0.4:
@@ -185,7 +106,7 @@ async def detect_image_annotated(file: UploadFile = File(...)):
 
         for i in indices:
             x, y, w, h = boxes[i]
-            label = COCO_CLASSES[class_ids[i] + 1] if class_ids[i] < len(COCO_CLASSES) else f"id {class_ids[i]}"
+            label = COCO_CLASSES[class_ids[i]] if class_ids[i] < len(COCO_CLASSES) else f"id {class_ids[i]}"
             conf = confidences[i]
 
             text = f"{label} ({conf:.2f})"
